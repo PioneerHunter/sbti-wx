9c138cbd168ad937632335708936fe925a770a86
Your Name <yunche@ksztone.com>
2026-04-10 19:22:45 +0800

    feat: 功能实现

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -3,6 +3,8 @@
 interface IAppOption {
   globalData: {
     userInfo?: WechatMiniprogram.UserInfo,
+    answers?: Record<string, number>,
+    shuffledQuestions?: Array<{ id: string; dim?: string; special?: boolean; kind?: string }>,
   }
   userInfoReadyCallback?: WechatMiniprogram.GetUserInfoSuccessCallback,
 }
